e368c41a0a1819178429dac733c5bd76d1b67805
Kazu Yamamoto <kazu@iij.ad.jp>
2011-08-31 16:14:28 +0900

    doc update.

--- Network/DNS.hs
+++ Network/DNS.hs
@@ -2,8 +2,7 @@
   Thread-safe DNS library written in Haskell.
 
   Currently, only resolver side is supported. This code is written in
-  Haskell, not using FFI. So, the \"-threaded\" option for GHC is not
-  necessary.
+  Haskell, not using FFI.
 
   Sample code:
 
