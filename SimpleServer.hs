a4621fb1c4f4aad268c351d06c9ba85315fe41c3
huangyi <huangy@jjmmw.com>
2011-10-24 14:01:14 +0800

    remove debug code and polish naming a little.

--- SimpleServer.hs
+++ SimpleServer.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE RecordWildCards, OverloadedStrings #-}
 
 import System.Environment
-import Debug.Trace
 import Control.Monad
 import Control.Concurrent
 import Control.Applicative
@@ -18,18 +17,23 @@ import Network.Socket hiding (recvFrom)
 import Network.Socket.ByteString
 import Network.Socket.Enumerator
 import Data.Default
+import Data.IP
 
 data Conf = Conf {
     bufSize :: Int
   , timeOut :: Int
   , realDNS :: HostName
+  , hosts   :: [(Domain, IPv4)]
 }
 
+-- TODO load hosts from config file
+-- TODO support wildcard in hosts
 instance Default Conf where
     def = Conf {
         bufSize = 512
       , timeOut = 3 * 1000 * 1000
       , realDNS = "192.168.1.1"
+      , hosts   = [("localhost", "127.0.0.1")]
     }
 
 timeout' :: String -> Int -> IO a -> IO (Maybe a)
@@ -40,12 +44,11 @@ timeout' msg tm io = do
 
 proxyRequest :: Conf -> ResolvConf -> DNSFormat -> IO (Maybe DNSFormat)
 proxyRequest Conf{..} rc req = do
-    let
-      worker Resolver{..} = do
-        let packet = mconcat . toChunks $ composeDNSFormat req
-        sendAll dnsSock packet
-        let responseEnum = enumSocket dnsBufsize dnsSock
-        parseResponse responseEnum responseIter
+    let worker Resolver{..} = do
+          let packet = mconcat . toChunks $ composeDNSFormat req
+          sendAll dnsSock packet
+          let responseEnum = enumSocket dnsBufsize dnsSock
+          parseResponse responseEnum responseIter
 
     rs <- makeResolvSeed rc
     withResolver rs $ \r ->
@@ -56,38 +59,31 @@ proxyRequest Conf{..} rc req = do
     check rsp = let hdr = header rsp
                 in  if identifier hdr == ident
                         then Just rsp
-                        else trace "identifier not match" Nothing
+                        else Nothing
 
-{--
- - TBD
- --}
 handleRequest :: Conf -> ResolvConf -> DNSFormat -> IO (Maybe DNSFormat)
-handleRequest conf rc req = maybe (proxyRequest conf rc req) (trace "return A record" $ return . Just) mresponse
+handleRequest conf@Conf{hosts=hosts} rc req =
+    maybe
+      (proxyRequest conf rc req)
+      (return . Just)
+      lookupHosts
   where
     filterA = filter ((==A) . qtype)
-    mresponse = do
-        let ident = identifier . header $ req
+    ident = identifier . header $ req
+    lookupHosts = do
         q <- listToMaybe . filterA . question $ req
-        let dom = qname q
-        ip <- lookup dom hosts
+        ip <- lookup (qname q) hosts
         return $ responseA ident q ip
-    hosts = [ ("proxy.com.", "127.0.0.1")
-            --, ("*.proxy.com", "127.0.0.1")
-            ]
 
 handlePacket :: Conf -> Socket -> SockAddr -> S.ByteString -> IO ()
 handlePacket conf@Conf{..} sock addr bs = case runDNSFormat_ (fromChunks [bs]) of
     Right req -> do
-        print req
         let rc = defaultResolvConf { resolvInfo = RCHostName realDNS }
         mrsp <- handleRequest conf rc req
-        print mrsp
         case mrsp of
             Just rsp ->
                 let packet = mconcat . toChunks $ composeDNSFormat rsp
-                in  timeout' "send timeout" timeOut (sendAllTo sock packet addr) >>
-                    print (S.length packet) >>
-                    return ()
+                in  void $ timeout' "send timeout" timeOut (sendAllTo sock packet addr)
             Nothing -> return ()
     Left msg -> putStrLn msg
 
