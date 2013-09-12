d7feac59465b537c63d9ca43e9e5068d7d1216c4
Michael Orlitzky <michael@orlitzky.com>
2013-09-12 09:47:48 -0400

    Remove non-haddock example from the header of Network.DNS. Mention thread-safety and purity in the Cabal description field. Document the re-exported modules in Network.DNS.

--- Network/DNS.hs
+++ Network/DNS.hs
@@ -1,29 +1,33 @@
-{-|
-  Thread-safe DNS library written in Haskell.
-
-  This code is written in Haskell, not using FFI.
-
-  Sample code for DNS lookup:
-
-@
-    import qualified Network.DNS as DNS (lookup)
-    import Network.DNS hiding (lookup)
-    main :: IO ()
-    main = do
-        rs <- makeResolvSeed defaultResolvConf
-        withResolver rs $ \\resolver -> do
-            DNS.lookup resolver \"www.example.com\" A >>= print
-@
--}
-
+-- | The Network.DNS module re-exports all other exposed modules for
+--   convenience.
+--
+--   Applications will most likely use the high-level interface, while
+--   library/daemon authors may need to use the lower-level one.
+--
 module Network.DNS (
   -- * High level
     module Network.DNS.Lookup
+  -- | The "Network.DNS.Lookup" module contains simple functions to
+  --   perform various DNS lookups. If you simply want to resolve a
+  --   hostname ('lookupA'), or find a domain's MX record
+  --   ('lookupMX'), this is the easiest way to do it.
+
   , module Network.DNS.Resolver
+  -- | The "Network.DNS.Resolver" module is slightly more low-level
+  --   than "Network.DNS.Lookup". If you need to do something unusual,
+  --   you may need to use the 'lookup', 'lookupAuth', or 'lookupRaw'
+  --   functions.
+
   , module Network.DNS.Types
+  -- | All of the types that the other modules use.
+
   -- * Low level
   , module Network.DNS.Decode
+  -- | Decoding a response.
+
   , module Network.DNS.Encode
+  -- | Encoding a query.
+
   ) where
 
 import Network.DNS.Lookup
@@ -31,5 +35,3 @@ import Network.DNS.Resolver
 import Network.DNS.Types
 import Network.DNS.Decode
 import Network.DNS.Encode
-
-
