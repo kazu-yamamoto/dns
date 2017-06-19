33f208b9c372181dac62d52ed203789f734a9f68
Erik de Castro Lopo <erikd@mega-nerd.com>
2017-06-19 20:41:41 +1000

    Add some more tests

--- test/DecodeSpec.hs
+++ test/DecodeSpec.hs
@@ -11,43 +11,60 @@ import Data.Word8
 import Foreign.ForeignPtr (withForeignPtr)
 import Foreign.Ptr (plusPtr)
 import Foreign.Storable (peek, poke, peekByteOff)
-import Network.DNS.Decode
-import Network.DNS.Encode
+import Network.DNS
 import Test.Hspec
 
 ----------------------------------------------------------------
 
 test_doublePointer :: BL.ByteString
 test_doublePointer = "f7eb8500000100010007000404736563330561706e696303636f6d0000010001c00c0001000100001c200004ca0c1c8cc0110002000100001c20000f036e73310561706e6963036e657400c0300002000100001c200006036e7333c040c0300002000100001c200006036e7334c040c0300002000100001c20001004736563310561706e696303636f6d00c0300002000100001c20001704736563310761757468646e730472697065036e657400c0300002000100001c20001004736563320561706e696303636f6d00c0300002000100001c2000070473656333c0bfc07b0001000100001c200004ca0c1d3bc07b001c000100001c20001020010dc02001000a4608000000000059c0ba0001000100001c200004ca0c1d3cc0d6001c000100001c20001020010dc0000100004777000000000140"
+-- DNSMessage {header = DNSHeader {identifier = 63467, flags = DNSFlags {qOrR = QR_Response, opcode = OP_STD, authAnswer = True, trunCation = False, recDesired = True, recAvailable = False, rcode = NoErr, authenData = False}}, question = [Question {qname = "sec3.apnic.com.", qtype = A}], answer = [ResourceRecord {rrname = "sec3.apnic.com.", rrtype = A, rrttl = 7200, rdata = 202.12.28.140}], authority = [ResourceRecord {rrname = "apnic.com.", rrtype = NS, rrttl = 7200, rdata = ns1.apnic.net.},ResourceRecord {rrname = "apnic.com.", rrtype = NS, rrttl = 7200, rdata = ns3.apnic.net.},ResourceRecord {rrname = "apnic.com.", rrtype = NS, rrttl = 7200, rdata = ns4.apnic.net.},ResourceRecord {rrname = "apnic.com.", rrtype = NS, rrttl = 7200, rdata = sec1.apnic.com.},ResourceRecord {rrname = "apnic.com.", rrtype = NS, rrttl = 7200, rdata = sec1.authdns.ripe.net.},ResourceRecord {rrname = "apnic.com.", rrtype = NS, rrttl = 7200, rdata = sec2.apnic.com.},ResourceRecord {rrname = "apnic.com.", rrtype = NS, rrttl = 7200, rdata = sec3.apnic.com.}], additional = [ResourceRecord {rrname = "sec1.apnic.com.", rrtype = A, rrttl = 7200, rdata = 202.12.29.59},ResourceRecord {rrname = "sec1.apnic.com.", rrtype = AAAA, rrttl = 7200, rdata = 2001:dc0:2001:a:4608::59},ResourceRecord {rrname = "sec2.apnic.com.", rrtype = A, rrttl = 7200, rdata = 202.12.29.60},ResourceRecord {rrname = "sec3.apnic.com.", rrtype = AAAA, rrttl = 7200, rdata = 2001:dc0:1:0:4777::140}]})
 
 test_txt :: BL.ByteString
 test_txt = "463181800001000100000000076e69636f6c6173046b766462076e647072696d6102696f0000100001c00c0010000100000e10000c6e69636f6c61732e6b766462"
+-- DNSMessage {header = DNSHeader {identifier = 17969, flags = DNSFlags {qOrR = QR_Response, opcode = OP_STD, authAnswer = False, trunCation = False, recDesired = True, recAvailable = True, rcode = NoErr, authenData = False}}
+--              , question = [Question {qname = "nicolas.kvdb.ndprima.io.", qtype = TXT}]
+--              , answer = [ResourceRecord {rrname = "nicolas.kvdb.ndprima.io.", rrtype = TXT, rrttl = 3600, rdata = icolas.kvdb}]
+--              , authority = []
+--              , additional = []})
 
 test_dname :: BL.ByteString
 test_dname = "b3c0818000010005000200010377777706376b616e616c02636f02696c0000010001c0100027000100000003000c0769737261656c3702727500c00c0005000100000003000603777777c02ec046000500010000255b0002c02ec02e000100010000003d000451daf938c02e000100010000003d0004c33ce84ac02e000200010005412b000c036e7332026137036f726700c02e000200010005412b0006036e7331c08a0000291000000000000000"
+-- DNSMessage {header = DNSHeader {identifier = 46016, flags = DNSFlags {qOrR = QR_Response, opcode = OP_STD, authAnswer = False, trunCation = False, recDesired = True, recAvailable = True, rcode = NoErr, authenData = False}}, question = [Question {qname = "www.7kanal.co.il.", qtype = A}], answer = [ResourceRecord {rrname = "7kanal.co.il.", rrtype = DNAME, rrttl = 3, rdata = israel7.ru.},ResourceRecord {rrname = "www.7kanal.co.il.", rrtype = CNAME, rrttl = 3, rdata = www.israel7.ru.},ResourceRecord {rrname = "www.israel7.ru.", rrtype = CNAME, rrttl = 9563, rdata = israel7.ru.},ResourceRecord {rrname = "israel7.ru.", rrtype = A, rrttl = 61, rdata = 81.218.249.56},ResourceRecord {rrname = "israel7.ru.", rrtype = A, rrttl = 61, rdata = 195.60.232.74}], authority = [ResourceRecord {rrname = "israel7.ru.", rrtype = NS, rrttl = 344363, rdata = ns2.a7.org.},ResourceRecord {rrname = "israel7.ru.", rrtype = NS, rrttl = 344363, rdata = ns1.a7.org.}], additional = [OptRecord {orudpsize = 4096, ordnssecok = False, orversion = 0, rdata = []}]})
+
+test_mx :: BL.ByteString
+test_mx = "f03681800001000100000001036d6577036f726700000f0001c00c000f000100000df10009000a046d61696cc00c0000291000000000000000"
+-- DNSMessage {header = DNSHeader {identifier = 61494, flags = DNSFlags {qOrR = QR_Response, opcode = OP_STD, authAnswer = False, trunCation = False, recDesired = True, recAvailable = True, rcode = NoErr, authenData = False}}
+--              , question = [Question {qname = "mew.org.", qtype = MX}]
+--              , answer = [ResourceRecord {rrname = "mew.org.", rrtype = MX, rrttl = 3569, rdata = 10 mail.mew.org.}]
+--              , authority = []
+--              , additional = [OptRecord {orudpsize = 4096, ordnssecok = False, orversion = 0, rdata = []}]})
 
 ----------------------------------------------------------------
 
 spec :: Spec
 spec = do
     describe "decode" $ do
-        it "decodes double pointers correctly" $ do
-            let Right x1 = decode $ fromHexString test_doublePointer
-                Right x2 = decode (encode x1)
-                Right x3 = decode (encode x2)
-            x3 `shouldBe` x2
-        it "decodes dname" $ do
-            let Right x1 = decode $ fromHexString test_dname
-                Right x2 = decode (encode x1)
-                Right x3 = decode (encode x2)
-            print x1
-            x3 `shouldBe` x2
-        it "decodes txt" $ do
-            let Right x1 = decode $ fromHexString test_txt
-                Right x2 = decode (encode x1)
-                Right x3 = decode (encode x2)
-            print x1
-            x3 `shouldBe` x2
+        it "decodes double pointers correctly" $
+            tripleDecodeTest test_doublePointer
+        it "decodes dname" $
+            tripleDecodeTest test_dname
+        it "decodes txt" $
+            tripleDecodeTest test_txt
+        it "decodes mx" $
+            tripleDecodeTest test_mx
+
+
+tripleDecodeTest :: BL.ByteString -> IO ()
+tripleDecodeTest hexbs =
+    ecase (decode $ fromHexString hexbs) fail $ \ x1 ->
+        ecase (decode $ encode x1) fail $ \ x2 ->
+            ecase (decode $ encode x2) fail $ \ x3 ->
+                x3 `shouldBe` x2
+
+ecase :: Either a b -> (a -> c) -> (b -> c) -> c
+ecase (Left a) f _ = f a
+ecase (Right b) _ g = g b
 
 ----------------------------------------------------------------
 
--- test2/LookupSpec.hs
+++ test2/LookupSpec.hs
@@ -6,13 +6,46 @@ import Network.DNS as DNS
 import Test.Hspec
 
 spec :: Spec
-spec = do
+spec = describe "lookup" $ do
 
-    describe "lookupAAAA" $ do
-        it "gets IPv6 addresses" $ do
-            rs <- makeResolvSeed defaultResolvConf
-            withResolver rs $ \resolver -> do
-                DNS.lookupAAAA resolver "mew.org"
-                    `shouldReturn`
-                    Right []
+    it "lookupA" $ do
+        rs <- makeResolvSeed defaultResolvConf
+        withResolver rs $ \resolver -> do
+            addrs <- DNS.lookupA resolver "mew.org"
+            -- mew.org has one or more IPv6 addresses
+            fmap null addrs `shouldBe` Right False
 
+    it "lookupAAAA" $ do
+        rs <- makeResolvSeed defaultResolvConf
+        withResolver rs $ \resolver -> do
+            -- google.com has one or more IPv6 addresses
+            addrs <- DNS.lookupAAAA resolver "google.com"
+            fmap null addrs `shouldBe` Right False
+
+    it "lookupAAAA with emty result" $ do
+        rs <- makeResolvSeed defaultResolvConf
+        withResolver rs $ \resolver -> do
+            addrs <- DNS.lookupAAAA resolver "mew.org"
+            -- mew.org does not have any IPv6 addresses
+            fmap null addrs `shouldBe` Right True
+
+    it "lookupMX" $ do
+        rs <- makeResolvSeed defaultResolvConf
+        withResolver rs $ \resolver -> do
+            addrs <- DNS.lookupMX resolver "mew.org"
+            -- mew.org has one or more MX records.
+            fmap null addrs `shouldBe` Right False
+
+    it "lookupTXT" $ do
+        rs <- makeResolvSeed defaultResolvConf
+        withResolver rs $ \resolver -> do
+            addrs <- DNS.lookupTXT resolver "mew.org"
+            -- mew.org has one or more TXT records.
+            fmap null addrs `shouldBe` Right False
+
+    it "lookupNS" $ do
+        rs <- makeResolvSeed defaultResolvConf
+        withResolver rs $ \resolver -> do
+            addrs <- DNS.lookupNS resolver "mew.org"
+            -- mew.org has one or more NS records.
+            fmap null addrs `shouldBe` Right False
