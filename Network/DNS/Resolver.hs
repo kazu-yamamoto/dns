08776b2e1553dffe8747ec2bf848c730db00378c
Alfredo Di Napoli <alfredo.dinapoli@iohk.io>
2017-08-30 11:58:13 +0200

    Fix sendAll on Windows.
    This commit fixes some lookup problems Windows users were facing when
    using the library. The culprit (as confirmed by some tests) was in
    the implementation of `sendAll`, that on Windows was defined as part of
    the code of this library, but this is not necessary anymore unless an
    old version of GHC (< 7) is being used.
    
    For modern environments, Network.Socket.ByteString.sendAll is all we
    need.

--- Network/DNS/Resolver.hs
+++ Network/DNS/Resolver.hs
@@ -19,6 +19,16 @@ module Network.DNS.Resolver (
   , fromDNSFormat
   ) where
 
+#if !defined(mingw32_HOST_OS)
+#define POSIX
+#else
+#define WIN
+#endif
+
+#if __GLASGOW_HASKELL__ < 709
+#define GHC6
+#endif
+
 import Control.Exception (bracket, throwIO)
 import Data.Char (isSpace)
 import Data.List (isPrefixOf)
@@ -37,11 +47,11 @@ import Prelude hiding (lookup)
 import System.Random (getStdRandom, random)
 import System.Timeout (timeout)
 import Data.Word (Word16)
-#if __GLASGOW_HASKELL__ < 709
+#ifdef GHC6
 import Control.Applicative ((<$>), (<*>), pure)
 #endif
 
-#if mingw32_HOST_OS == 1
+#if defined(WIN) && defined(GHC6)
 import Network.Socket (send)
 import qualified Data.ByteString.Char8 as BS
 import Control.Monad (when)
@@ -453,9 +463,8 @@ tcpLookup query peer tm (Just vc) = do
         Nothing  -> return $ Left TimeoutExpired
         Just res -> return $ Right res
 
-#if mingw32_HOST_OS == 1
--- Windows does not support sendAll in Network.ByteString.
--- This implements sendAll with Haskell Strings.
+#if defined(WIN) && defined(GHC6)
+-- Windows does not support sendAll in Network.ByteString for older GHCs.
 sendAll :: Socket -> BS.ByteString -> IO ()
 sendAll sock bs = do
   sent <- send sock (BS.unpack bs)
