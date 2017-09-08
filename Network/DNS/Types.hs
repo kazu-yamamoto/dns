14ba0823d7fb26c8a5609c699f39b3881e930559
Viktor Dukhovni <ietf-dane@dukhovni.org>
2017-09-08 01:24:25 +0000

    Add RFC2136 (Dynamic Update) RCODEs

--- Network/DNS/Types.hs
+++ Network/DNS/Types.hs
@@ -63,6 +63,11 @@ module Network.DNS.Types (
   , NameErr
   , NotImpl
   , Refused
+  , YXDomain
+  , YXRRSet
+  , NXRRSet
+  , NotAuth
+  , NotZone
   , BadOpt
   )
   , toRCODE
@@ -377,6 +382,26 @@ pattern NotImpl    = RCODE  4
 --   transfer) for particular data.
 pattern Refused   :: RCODE
 pattern Refused    = RCODE  5
+-- | YXDomain - Dynamic update response, a pre-requisite domain that should not
+-- exist, does exist.
+pattern YXDomain :: RCODE
+pattern YXDomain  = RCODE 6
+-- | YXRRSet - Dynamic update response, a pre-requisite RRSet that should not
+-- exist, does exist.
+pattern YXRRSet  :: RCODE
+pattern YXRRSet   = RCODE 7
+-- | NXRRSet - Dynamic update response, a pre-requisite RRSet that should
+-- exist, does not exist.
+pattern NXRRSet  :: RCODE
+pattern NXRRSet   = RCODE 8
+-- | NotAuth - Dynamic update response, the server is not authoritative for the
+-- zone named in the Zone Section.
+pattern NotAuth  :: RCODE
+pattern NotAuth   = RCODE 9
+-- | NotZone - Dynamic update response, a name used in the Prerequisite or
+-- Update Section is not within the zone denoted by the Zone Section.
+pattern NotZone  :: RCODE
+pattern NotZone   = RCODE 10
 -- | Bad OPT Version (RFC 6891) or TSIG Signature Failure (RFC2845).
 pattern BadOpt    :: RCODE
 pattern BadOpt     = RCODE 16
@@ -388,6 +413,10 @@ instance Show RCODE where
     show NameErr   = "NameErr"
     show NotImpl   = "NotImpl"
     show Refused   = "Refused"
+    show YXDomain  = "YXDomain"
+    show YXRRSet   = "YXRRSet"
+    show NotAuth   = "NotAuth"
+    show NotZone   = "NotZone"
     show BadOpt    = "BadOpt"
     show x         = "RCODE " ++ (show $ fromRCODE x)
 
