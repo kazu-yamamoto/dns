be31b3aeec118379e764322c95f4a6524d404c96
Kazu Yamamoto <kazu@iij.ad.jp>
2017-11-07 10:39:39 +0900

    defining resolvParallel.

--- Network/DNS/Types/Internal.hs
+++ Network/DNS/Types/Internal.hs
@@ -38,13 +38,15 @@ data FileOrNumericHost = RCFilePath FilePath -- ^ A path for \"resolv.conf\"
 --  >>> let conf = defaultResolvConf { resolvEDNS = [fromEDNS0 defaultEDNS0 { udpSize = 1280 }] }
 data ResolvConf = ResolvConf {
    -- | Server information.
-    resolvInfo    :: FileOrNumericHost
+    resolvInfo     :: FileOrNumericHost
    -- | Timeout in micro seconds.
-  , resolvTimeout :: Int
+  , resolvTimeout  :: Int
    -- | The number of retries including the first try.
-  , resolvRetry   :: Int
+  , resolvRetry    :: Int
    -- | Additional resource records to specify EDNS.
-  , resolvEDNS    :: [ResourceRecord]
+  , resolvEDNS     :: [ResourceRecord]
+   -- | Parallel queries if multiple DNS servers are specified.
+  , resolvParallel :: Bool
 } deriving Show
 
 -- | Return a default 'ResolvConf':
@@ -58,10 +60,11 @@ data ResolvConf = ResolvConf {
 --     * 'resolvEDNS' is EDNS0 with a 4,096-bytes buffer.
 defaultResolvConf :: ResolvConf
 defaultResolvConf = ResolvConf {
-    resolvInfo    = RCFilePath "/etc/resolv.conf"
-  , resolvTimeout = 3 * 1000 * 1000
-  , resolvRetry   = 3
-  , resolvEDNS    = [fromEDNS0 defaultEDNS0]
+    resolvInfo     = RCFilePath "/etc/resolv.conf"
+  , resolvTimeout  = 3 * 1000 * 1000
+  , resolvRetry    = 3
+  , resolvEDNS     = [fromEDNS0 defaultEDNS0]
+  , resolvParallel = False
 }
 
 ----------------------------------------------------------------
