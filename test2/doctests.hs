22fab9168db758d36dab33bb353c28d9a8c9ddab
Viktor Dukhovni <ietf-dane@dukhovni.org>
2018-12-20 11:33:30 -0500

    Temporarily make doctest more verbose

--- test2/doctests.hs
+++ test2/doctests.hs
@@ -3,10 +3,17 @@ module Main where
 import Build_doctests (flags, pkgs, module_sources)
 import Data.Foldable (traverse_)
 import Test.DocTest (doctest)
+import System.IO (hFlush, hPutStrLn, stderr)
 
 main :: IO ()
 main = do
-    traverse_ putStrLn args
+    traverse_ (hPutStrLn stderr) args
+    hFlush stderr
     doctest args
   where
-    args = ["-XOverloadedStrings", "-XCPP"] ++ flags ++ pkgs ++ module_sources
+    args = [ "-XOverloadedStrings"
+           , "-XCPP"
+           , "--verbose" ] ++
+           flags ++
+           pkgs ++
+           module_sources
