97e6adf6cc50238120cb7206a2d3da03b8fb1ae9
Viktor Dukhovni <ietf-dane@dukhovni.org>
2019-10-12 01:52:44 -0400

    testing

--- test2/doctests.hs
+++ test2/doctests.hs
@@ -1,14 +1,40 @@
+{-# LANGUAGE CPP #-}
+
 module Main where
 
-import Build_doctests (flags, pkgs, module_sources)
-import Test.DocTest (doctest)
+import Test.DocTest
 
 main :: IO ()
-main = do
-    putStrLn $ unwords $ "\ndoctest args: " : args
-    doctest args
-  where
-    args = [ "-XCPP" ] ++
-           flags ++
-           pkgs ++
-           module_sources
+main = doctest [
+    "-XOverloadedStrings"
+  , "-XCPP"
+  {-
+    Both 'iproute' and 'network-data' provide
+    ‘Data.IP’ package:
+      Ambiguous interface for ‘Data.IP’:
+        it was found in multiple packages: network-data-0.5.3 iproute-1.7.0
+    We ignore network-data to make tests pass.
+  -}
+  , "-package=array"
+  , "-package=async"
+  , "-package=attoparsec"
+  , "-package=auto-update"
+  , "-package=base16-bytestring"
+  , "-package=base64-bytestring"
+  , "-package=bytestring"
+  , "-package=containers"
+  , "-package=cryptonite"
+  , "-package=hourglass"
+  , "-package=iproute"
+  , "-package=mtl"
+  , "-package=network"
+  , "-package=psqueues"
+  , "Network/DNS.hs"
+  , "Network/DNS/Decode/Parsers.hs"
+  , "Network/DNS/Lookup.hs"
+  , "Network/DNS/LookupRaw.hs"
+  , "Network/DNS/Resolver.hs"
+  , "Network/DNS/Types.hs"
+  , "Network/DNS/Types/Internal.hs"
+  , "Network/DNS/Utils.hs"
+  ]
