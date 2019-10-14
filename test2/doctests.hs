6d8b21581256cb09da61be7a785508f761e02d4e
Viktor Dukhovni <ietf-dane@dukhovni.org>
2019-10-14 19:53:33 -0400

    Add -liphlppi to doctests on Windows

--- test2/doctests.hs
+++ test2/doctests.hs
@@ -2,11 +2,12 @@ module Main where
 
 import Build_doctests (flags, pkgs, module_sources)
 import Test.DocTest (doctest)
+import System.Environment (getArgs)
 
 main :: IO ()
 main = do
     putStrLn $ unwords $ "\ndoctest args: " : args
-    doctest args
+    getArgs >>= doctest . (++) args
   where
     args = [ "-XCPP" ] ++
            flags ++
