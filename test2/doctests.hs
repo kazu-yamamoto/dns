e46d2b00085395c46b1d14fc9875c0a77efcf710
Viktor Dukhovni <postfix-users@dukhovni.org>
2019-10-20 17:50:52 +0200

    Portably include package=dns in doctests
    This configuration passes "cabal v1-test", "cabal v2-test" and
    "stack test", and *builds* and *runs* doctests on Windows, which
    then fail, but at least the "cbits" code is now loaded, so it could
    start working later, when the source of the hang is resolved.
    
    Sadly, it precludes using  "internal" libraries, which are not
    compatible with a Custom build at present.
    
    Which do we want more?  An internal library or "cabal v1-test"?  By
    using "cabal-doctest" all the required package-db, ... flags are
    passed in, making "-package=dns" work.

--- test2/doctests.hs
+++ test2/doctests.hs
@@ -1,4 +1,13 @@
+{-# LANGUAGE CPP #-}
 module Main where
+
+-- | Run doctests only on non-Windows systems.
+--
+-- The Windows doctests now compile and run, but either succeed quickly or
+-- randomly hang (before AppVeyor kills them after an hour).  So we disable
+-- these for now.  This can be tested again at some point in the future.
+--
+#if !defined(mingw32_HOST_OS)
 
 import Build_doctests (flags, pkgs, module_sources)
 import Test.DocTest (doctest)
@@ -10,5 +19,11 @@ main = do
   where
     args = [ "-XCPP" ] ++
            flags ++
-           pkgs ++
+           pkgs ++ ["-package=dns"] ++
            module_sources
+#else
+
+main :: IO ()
+main = return ()
+
+#endif
