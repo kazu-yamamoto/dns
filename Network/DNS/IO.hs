1d26a12651c2f559a0d1eca43614dfe2a4560ee4
Josh Brown <josh@epidev.com>
2021-12-20 23:39:43 +1100

    receiveFrom, sendTo fns for UDP. Bumped LTS version

--- Network/DNS/IO.hs
+++ Network/DNS/IO.hs
@@ -3,9 +3,11 @@
 module Network.DNS.IO (
     -- * Receiving DNS messages
     receive
+  , receiveFrom
   , receiveVC
     -- * Sending pre-encoded messages
   , send
+  , sendTo
   , sendVC
   , sendAll
     -- ** Encoding queries for transmission
@@ -24,8 +26,8 @@ import qualified Data.ByteString.Lazy.Char8 as LBS
 import Data.IP (IPv4, IPv6)
 import Time.System (timeCurrent)
 import Time.Types (Elapsed(..), Seconds(..))
-import Network.Socket (Socket)
-import Network.Socket.ByteString (recv)
+import Network.Socket (Socket, SockAddr)
+import Network.Socket.ByteString (recv, recvFrom)
 import qualified Network.Socket.ByteString as Socket
 import System.IO.Error
 
@@ -36,19 +38,30 @@ import Network.DNS.Types.Internal
 
 ----------------------------------------------------------------
 
+-- | Receive and decode a single 'DNSMessage' from a UDP 'Socket', throwing away
+-- the client address.  Messages longer than 'maxUdpSize' are silently
+-- truncated, but this should not occur in practice, since we cap the advertised
+-- EDNS UDP buffer size limit at the same value.  A 'DNSError' is raised if I/O
+-- or message decoding fails.
+--
+receive :: Socket -> IO DNSMessage
+receive sock = do
+    (msg, _) <- receiveFrom sock
+    return msg
+
 -- | Receive and decode a single 'DNSMessage' from a UDP 'Socket'.  Messages
 -- longer than 'maxUdpSize' are silently truncated, but this should not occur
 -- in practice, since we cap the advertised EDNS UDP buffer size limit at the
 -- same value.  A 'DNSError' is raised if I/O or message decoding fails.
 --
-receive :: Socket -> IO DNSMessage
-receive sock = do
+receiveFrom :: Socket -> IO (DNSMessage, SockAddr)
+receiveFrom sock = do
     let bufsiz = fromIntegral maxUdpSize
-    bs <- recv sock bufsiz `E.catch` \e -> E.throwIO $ NetworkFailure e
+    (bs, client) <- recvFrom sock bufsiz `E.catch` \e -> E.throwIO $ NetworkFailure e
     Elapsed (Seconds now) <- timeCurrent
     case decodeAt now bs of
         Left  e   -> E.throwIO e
-        Right msg -> return msg
+        Right msg -> return (msg, client)
 
 -- | Receive and decode a single 'DNSMesage' from a virtual-circuit (TCP).  It
 -- is up to the caller to implement any desired timeout. An 'DNSError' is
@@ -103,6 +116,15 @@ recvDNS sock len = recv1 `E.catch` \e -> E.throwIO $ NetworkFailure e
 send :: Socket -> ByteString -> IO ()
 send = (void .). Socket.send
 {-# INLINE send #-}
+
+-- | Send an encoded 'DNSMessage' datagram over UDP to a given address.  The
+-- message length is implicit in the size of the UDP datagram.  With TCP you
+-- must use 'sendVC', because TCP does not have message boundaries, and each
+-- message needs to be prepended with an explicit length.
+--
+sendTo :: Socket -> ByteString -> SockAddr -> IO ()
+sendTo sock str addr = Socket.sendTo sock str addr >> return ()
+{-# INLINE sendTo #-}
 
 -- | Send a single encoded 'DNSMessage' over TCP.  An explicit length is
 -- prepended to the encoded buffer before transmission.  If you want to
